ff3d92f983a1a5a24545ecc873445a73f5cd4f3a
Mugabodenys <mugabodan2020@gmail.com>
2023-05-18 16:18:47 +0200

    changed comments

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -6,51 +6,38 @@
  * @returns {number}
  */
 export default function penaltyPoints(password = "") {
-  // If password is empty, return 0 penalty points
-  if (!password) 
-    return 0;
-    
   
-  // If password is not a string, convert it to a string
+  if (!password) {
+    return 0;
+  }  
+  
   if (typeof password !== "string") password = String(password);
 
-  let prevOccur = '';
-  let nOfOccurrence = 0;
-  let points = 0;
+    let prevOccur = '';
+    let nOfOccurrence = 0;
+    let points = 0;
 
-  // Iterate over each character in the password
-  for (let i = 0; i < password.length; i++) {
-    const char = password[i];
-
-    // If the current character is the same as the previous character, increment the occurrence count
-    if (char === prevOccur) {
-      nOfOccurrence++;
-    } else {
-      // If there were exactly 2 occurrences of the previous character, add 1 point
-      if (nOfOccurrence === 2) {
-        points++;
+    for (let i = 0; i < password.length; i++) {
+      const char = password[i];
+  
+      if (char === prevOccur) {
+        nOfOccurrence++;
+      } else {
+        if (nOfOccurrence === 2) {
+          points++;
+        } else if (nOfOccurrence >= 3) {
+          points += 2;
+        } 
+  
+        prevOccur = char;
+        nOfOccurrence = 1;
       }
-      // If there were 3 or more occurrences of the previous character, add 2 points
-      else if (nOfOccurrence >= 3) {
-        points += 2;
-      } 
-
-      // Update the previous character and reset the occurrence count
-      prevOccur = char;
-      nOfOccurrence = 1;
     }
-  }
-
-  // Check the occurrence count after the loop ends
-  // If there were exactly 2 occurrences of the last character, add 1 point
-  if (nOfOccurrence === 2) {
-    points++;
-  }
-  // If there were 3 or more occurrences of the last character, add 2 points
-  else if (nOfOccurrence >= 3) {
-    points += 2;
-  } 
-
-  return points;
+  
+    if (nOfOccurrence === 2) {
+      points++;
+    } else if (nOfOccurrence >= 3) {
+      points += 2;
+    } 
+    return points;
 }
-
